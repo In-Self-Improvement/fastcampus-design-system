f057b129647ea746849a49f69cf0611dc2107953
bong <borobong230@gmail.com>
2024-05-16 18:44:44 +0900

    :tada: [Init] esbuild setting for design system #HEAD

---
+++ packages/themes/dist/index.cjs
@@ -0,0 +1,2 @@
+"use strict";var m=Object.defineProperty;var b=Object.getOwnPropertyDescriptor;var o=Object.getOwnPropertyNames;var c=Object.prototype.hasOwnProperty;var f=(r,n)=>{for(var e in n)m(r,e,{get:n[e],enumerable:!0})},i=(r,n,e,t)=>{if(n&&typeof n=="object"||typeof n=="function")for(let u of o(n))!c.call(r,u)&&u!==e&&m(r,u,{get:()=>n[u],enumerable:!(t=b(n,u))||t.enumerable});return r};var p=r=>i(m({},"__esModule",{value:!0}),r);var x={};f(x,{sum:()=>s});module.exports=p(x);function s(r,n){return r+n}
+//# sourceMappingURL=index.cjs.map
---
+++ packages/themes/dist/index.js
@@ -0,0 +1,2 @@
+function u(n,r){return n+r}export{u as sum};
+//# sourceMappingURL=index.js.map
