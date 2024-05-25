f7237e78cc01de2413cd69d7bb8a543f60324a11
Bong <borobong230@gmail.com>
2024-05-25 22:47:50 +0900

    :sparkles: [Feat] Add box system #main

---
+++ packages/themes/src/variables/box.ts
@@ -0,0 +1,25 @@
+//make spacing object 0 to 96 0 value is 0 and 96 value is 24rem
+export const spacing = {
+  0: "0",
+  1: "0.25rem",
+  2: "0.5rem",
+  3: "0.75rem",
+  4: "1rem",
+  5: "1.25rem",
+  6: "1.5rem",
+  7: "2.25rem",
+};
+
+export const radii = {
+  none: "0",
+  sm: "0.125rem",
+  base: "0.25rem",
+  md: "0.375rem",
+  lg: "0.5rem",
+  xl: "0.75rem",
+};
+
+export const shadows = {
+  xs: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+  sm: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+};
