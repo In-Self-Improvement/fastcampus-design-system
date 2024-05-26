5161009b234314fb40b111f17b1913a96293c302
Bong <borobong230@gmail.com>
2024-05-26 10:20:45 +0900

    :wrench: [Chore] Setting environment about mono repo #main

---
+++ .nx/cache/16258116311815560263/outputs/packages/themes/dist/classes/typography.d.ts
@@ -0,0 +1,42 @@
+export declare const heading: {
+    "4xl": {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    "3xl": {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    "2xl": {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    xl: {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    lg: {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    md: {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    sm: {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+    xs: {
+        fontSize: string;
+        fontWeight: string;
+        lineHeight: string;
+    };
+};
