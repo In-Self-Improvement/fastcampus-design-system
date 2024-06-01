87e0f1cda5f5f36faba2f3d997e5c5c8eb8ffcca
Bong <borobong230@gmail.com>
2024-06-02 00:12:45 +0900

    :wrench: [Chore] tsconfig base 설정 #main

---
+++ .nx/cache/1215039730068651549/outputs/packages/themes/dist/variables/box.d.ts
@@ -0,0 +1,22 @@
+export declare const spacing: {
+    0: string;
+    1: string;
+    2: string;
+    3: string;
+    4: string;
+    5: string;
+    6: string;
+    7: string;
+};
+export declare const radii: {
+    none: string;
+    sm: string;
+    base: string;
+    md: string;
+    lg: string;
+    xl: string;
+};
+export declare const shadows: {
+    xs: string;
+    sm: string;
+};
