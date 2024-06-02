fc66afd9bc4634a89968758bb48a78925ba8bbe5
Bong <borobong230@gmail.com>
2024-06-02 12:45:41 +0900

    :hammer: [Update] 240602 스터디 ~3.5 #main

---
+++ .nx/cache/9103782278306986967/outputs/packages/themes/dist/variables/colors/scale.d.ts
@@ -0,0 +1,71 @@
+export declare const whiteAlpha: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
+export declare const blackAlpha: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
+export declare const gray: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
+export declare const red: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
+export declare const orange: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+    900: string;
+};
+export declare const yellow: {
+    50: string;
+    100: string;
+    200: string;
+    300: string;
+    400: string;
+    500: string;
+    600: string;
+    700: string;
+    800: string;
+};
---
+++ packages/react/components/layout/dist/index.js
@@ -0,0 +1,2 @@
+var o="w1j8jr0";import{jsx as l}from"react/jsx-runtime";var r=()=>l("div",{className:o,children:"\uC548\uB155\uD558\uC138\uC694."});export{r as Block};
+//# sourceMappingURL=index.js.map
